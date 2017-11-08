d4cddf150a008e0097f2734d6fead5afe2e0de62
Docker Lu <pdfreport2excel@gmail.com>
2017-11-07 22:17:57 -0500

    Initial commit

--- routes.py
+++ routes.py
@@ -58,4 +58,5 @@ def get_sn():
 #    return render_template('static.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    port = int(os.environ.get("PORT", 5000))
+	app.run(debug=True, host='0.0.0.0', port=port)
